207562b8b2b4e1858c60a22a155c2d001aef2325
James Rinella <rinellaj2448@gmail.com>
2023-08-29 06:10:20 -0500

    switched to material-ui from bootstrap

--- src/components/AnswerBank.tsx
+++ src/components/AnswerBank.tsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import { AnswerProps } from "../props/Answer.types";
 import { Answer } from "./Answer";
 import { AnswerBankProps } from "../props/AnswerBank.types";
+import { Grid } from "@mui/material";
 
 export const AnswerBank = ({ answers }: AnswerBankProps) => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
-    <ul className="list-group">
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
       {answers.map((answer, index) => (
-        <li className={selectedIndex === index ? "list-group-item active" : "list-group-item"} key={index}>
+        <Grid xs={6}>
           <Answer
             isCorrect={answer.isCorrect}
             prefix={String.fromCharCode(index + 65)}
@@ -18,8 +19,26 @@ export const AnswerBank = ({ answers }: AnswerBankProps) => {
               setSelectedIndex(index);
             }}
           ></Answer>
-        </li>
+        </Grid>
       ))}
-    </ul>
+    </Grid>
+    // <Grid xs={6}>A</Grid>
+    // <Grid xs={6}>B</Grid>
+    // <Grid xs={6}>C</Grid>
+    // <Grid xs={6}>D</Grid>
+    // <ul className="list-group">
+    //   {answers.map((answer, index) => (
+    //     <li className={selectedIndex === index ? "list-group-item active" : "list-group-item"} key={index}>
+    //       <Answer
+    //         isCorrect={answer.isCorrect}
+    //         prefix={String.fromCharCode(index + 65)}
+    //         label={answer.answer}
+    //         onSelect={() => {
+    //           setSelectedIndex(index);
+    //         }}
+    //       ></Answer>
+    //     </li>
+    //   ))}
+    // </ul>
   );
 };
